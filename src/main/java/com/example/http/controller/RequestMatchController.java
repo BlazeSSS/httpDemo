b66cdd27a3994711d201e0f431563dbf7c4de42a
BlazeSSS <1136296108@qq.com>
2021-07-05 16:31:03 +0800

    first commit

---
+++ src/main/java/com/example/http/controller/RequestMatchController.java
@@ -0,0 +1,79 @@
+package com.example.http.controller;
+
+import com.example.http.model.FormModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RequestMapping("requestMatch")
+@RestController
+@CrossOrigin
+public class RequestMatchController {
+
+    /**
+     * GET
+     * @param query
+     * @return
+     */
+    @GetMapping("getForDefault")
+    public String getForDefault(String query) {
+        return "query=" + query;
+    }
+
+    @GetMapping("getForDefaultModel")
+    public String getForDefaultModel(FormModel model) {
+        return "model=" + model;
+    }
+
+    @GetMapping("getForRequestParam")
+    public String getForRequestParam(@RequestParam("query") String querys) {
+        return "query=" + querys;
+    }
+
+    /**
+     * POST JSON
+     * @param model
+     * @param query
+     * @return
+     */
+    @PostMapping("postJsonForDefaultModel")
+    public String postJsonForDefaultModel(FormModel model, String query) {
+        return "model=" + model + "; query=" + query;
+    }
+
+    @PostMapping("postJsonForDefaultMap")
+    public String postJsonForDefaultMap(Map<String, Object> params, String query) {
+        return "param=" + params + "; query=" + query;
+    }
+
+    @PostMapping("postJsonForRequestBodyMap")
+    public String postJsonForRequestBodyMap(@RequestBody Map<String, Object> params, String query) {
+        return "param=" + params + "; query=" + query;
+    }
+
+    @PostMapping("postJsonForRequestBodyModel")
+    public String postJsonForRequestBodyModel(@RequestBody FormModel model, String query) {
+        return "model=" + model + "; query=" + query;
+    }
+
+    /**
+     * POST Form
+     * @param param
+     * @param query
+     * @return
+     */
+    @PostMapping("postFormForDefault")
+    public String postFormForDefault(String param, String query) {
+        return "param=" + param + "; query=" + query;
+    }
+
+    @PostMapping("postFormForDefaultModel")
+    public String postFormForDefaultModel(FormModel model, String query) {
+        return "model=" + model + "; query=" + query;
+    }
+
+    @PostMapping("postFormForDefaultMap")
+    public String postFormForDefaultMap(Map<String, Object> params, String query) {
+        return "params=" + params + "; query=" + query;
+    }
+}
---
+++ src/main/java/com/example/http/model/FormModel.java
@@ -0,0 +1,11 @@
+package com.example.http.model;
+
+import lombok.Data;
+
+@Data
+public class FormModel {
+
+    private String query;
+
+    private String param;
+}
